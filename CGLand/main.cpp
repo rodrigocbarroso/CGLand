85ea47299dbdee1520db86aa185f730a7d62b4d9
Rodrigo Cunha <rodrigocunha@rodrigos-imac.home>
2017-02-22 18:52:43 -0300

    Initial Commit

---
+++ CGLand/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CGLand
+//
+//  Created by Rodrigo Cunha on 22/02/17.
+//  Copyright © 2017 Rodrigo Cunha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
